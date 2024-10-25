6a08533849b018233da42a46500a272964449139
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-10-25 12:07:31 +0100

    Creating other pages and importing them in the App.js componet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Home from "./components/pages/Home";
+import SignIn from "./components/pages/SignIn";
+import ContactUs from "./components/pages/ContactUs";
+import PageNotFound from "./components/pages/404";
 
 function App() {
   return (
