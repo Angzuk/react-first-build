73e69f705aecf06fc040d3c17d70ba15acbaca11
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-15 19:44:43 +0000

    Puting incoming object into state

--- src/components/entites/modules/ModuleForm.js
+++ src/components/entites/modules/ModuleForm.js
@@ -1,15 +1,18 @@
+import { useState } from 'react';
+
 const emptyModule = {
     ModuleName: "",
     ModuleCode: "",
     ModuleLevel: 0,
     ModuleYearID: 0,
     ModuleLeaderID: 0,
-    ModuleImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg";
+    ModuleImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg",
 }
 
 export default function ModuleForm({ initialmodule=emptyModule}){
     // Initialisation --------------------------------------------
     // State -----------------------------------------------------
+    const [module, setModule] = useState(initialmodule);
     // Handlers --------------------------------------------------
     // View ------------------------------------------------------
     return(
