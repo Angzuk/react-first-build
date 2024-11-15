76275eb82272646bbf4c303657fb5fd572665f91
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-15 19:38:42 +0000

    creating an empty module

--- src/components/entites/modules/ModuleForm.js
+++ src/components/entites/modules/ModuleForm.js
@@ -1,4 +1,11 @@
-
+const emptyModule = {
+    ModuleName: "",
+    ModuleCode: "",
+    ModuleLevel: 0,
+    ModuleYearID: 0,
+    ModuleLeaderID: 0,
+    ModuleImageURL: "https://images.freeimages.com/images/small-previews/293/cable-4-1243085.jpg";
+}
 
 export default function ModuleForm(){
     // Initialisation --------------------------------------------
