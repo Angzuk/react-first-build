74da1ea60f9f2bec9de18c7fbead02dcd8ab72dd
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-17 14:28:10 +0000

    Updating validation

--- src/components/entites/modules/ModuleForm.js
+++ src/components/entites/modules/ModuleForm.js
@@ -17,7 +17,7 @@ export default function ModuleForm({ initialmodule=emptyModule}){
         ModuleName: (name) => name.length>8,
         ModuleCode: (code) => /^\D{2}\d{4}$/.test(code),
         ModuleLevel: (level) => (level > 2) && (level < 8),
-        ModuleYearID: (id) => true,
+        ModuleYearID: (id) => id !==0,
         ModuleLeaderID: (id) => true,
         ModuleImageURL: (url) => /^(http|https):\/\/(([a-zA-Z0-9$\-_.+!*'(),;:&=]|%[0-9a-fA-F]{2})+@)?(((25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])){3})|localhost|([a-zA-Z0-9\-\u00C0-\u017F]+\.)+([a-zA-Z]{2,}))(:[0-9]+)?(\/(([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*(\/([a-zA-Z0-9$\-_.+!*'(),;:@&=]|%[0-9a-fA-F]{2})*)*)?(\?([a-zA-Z0-9$\-_.+!*'(),;:@&=/?]|%[0-9a-fA-F]{2})*)?(#([a-zA-Z0-9$\-_.+!*'(),;:@&=/?]|%[0-9a-fA-F]{2})*)?)?$/.test(url)    }
 
