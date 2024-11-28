4167c1c97453384b8d27430ff32944dfdd42c22b
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-28 10:20:25 +0000

    Making function useLoad reusable - instead of supplying endpoint we use variable that can be changed and passing it as a prop to the function

--- src/components/api/useLoad.js
+++ src/components/api/useLoad.js
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
+import API from "./API";
 
-export default function useLoad() {
+export default function useLoad(endpoint) {
 
     // State ---------------------------------------------------------------------------------------------------------------
     const [records, setRecords] = useState(null);
     const [loadingMessage, setLoadingMessage] = useState('Loading records ...');
 
     // Methods -------------------------------------------------------------------------------------------------------------
-    const loadRecords = async () => {
-        const response = await API.get(`/modules`);
+    const loadRecords = async (endpoint) => {
+        const response = await API.get(endpoint);
         response.isSuccess
             ? setRecords(response.result)
             : setLoadingMessage(response.message)
