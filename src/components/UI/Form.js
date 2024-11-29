55f4d5a58db60c7f2c7f89295692ce2b3a4f2d96
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-29 01:35:11 +0000

    Updating file Form.js

--- src/components/UI/Form.js
+++ src/components/UI/Form.js
@@ -1,75 +1,80 @@
-import { useState } from "react";
-import "./Form.scss";
-import ToolTipDecorator from "./ToolTipDecorator";
-import { ActionTray, ActionAdd, ActionClose } from "./Actions";
+import { useState } from 'react';
+import { ActionTray, ActionAdd, ActionClose } from './Actions.js';
+import ToolTipDecorator from './ToolTipDecorator.js';
+import './Form.scss';
 
 export default function Form({ children, onSubmit, onCancel }) {
-  // Initialisation ---------------------------------------
-  // Hooks ------------------------------------------------
-  // State ------------------------------------------------
-  // Context ----------------------------------------------
-  // Handlers ---------------------------------------------
-  const handleSubmit = () => onSubmit;
-  const handleCancel = () => onCancel;
+  // Initialisation ------------------------------
+  // Hooks ---------------------------------------
+  // State ---------------------------------------
+  // Context -------------------------------------
+  // Handlers ------------------------------------
+  const handleSubmit = () => onSubmit();
+  const handleCancel = () => onCancel();
 
-  // View -------------------------------------------------
-  return (
+  // View ----------------------------------------
+  return (  
     <form className="BorderedForm">
-      <div className="FormTray">{children}</div>
+
+      <div className="FormTray">
+        {
+          children
+        }
+      </div>
 
       <ActionTray>
         <ToolTipDecorator message="Submit record">
-          <ActionAdd showText onClick={handleSubmit} buttonText="Submit" />
+          <ActionAdd showText onClick={handleSubmit} buttonText="Submit"/>
         </ToolTipDecorator>
         <ToolTipDecorator message="Cancel form">
           <ActionClose showText onClick={handleCancel} buttonText="Cancel" />
         </ToolTipDecorator>
       </ActionTray>
+
     </form>
   );
 }
 
 function Item({ children, label, htmlFor, advice, error }) {
-  // Initialisation ---------------------------------------
-  // Hooks ------------------------------------------------
-  // State ------------------------------------------------
-  // Context ----------------------------------------------
-  // Handlers ---------------------------------------------
-  // View -------------------------------------------------
+  // Initialisation ------------------------------
+  // Hooks ---------------------------------------
+  // State ---------------------------------------
+  // Context -------------------------------------
+  // Handlers ------------------------------------
+  // View ----------------------------------------
   return (
     <div className="FormItem">
-      <label className="FormLabel" htmlFor={htmlFor}>
-        {label}
-      </label>
-      {advice && <p className="FormAdvice">{advice}</p>}
-      {children}
-      {error && <p className="FormError">{error}</p>}
+      <label className="FormLabel" htmlFor={htmlFor}>{label}</label>
+      {
+        advice && <p className="FormAdvice">{advice}</p>
+      }
+      {
+        children
+      }
+      {
+        error && <p className="FormError">{error}</p>
+      }
     </div>
   );
 }
 
 function useForm(initialRecord, conformance, { isValid, errorMessage }, onCancel, onSubmit) {
-  // Initialisation ---------------------------------------
-  // State ------------------------------------------------
+  // Initialisation ------------------------------
+  // State ---------------------------------------
   const [record, setRecord] = useState(initialRecord);
   const [errors, setErrors] = useState(
-    Object.keys(initialRecord).reduce(
-      (accum, key) => ({ ...accum, [key]: null }),
-      {}
-    )
+    Object.keys(initialRecord).reduce((accum, key) => ({ ...accum, [key]: null }), {})
   );
 
-  // Context ----------------------------------------------
-  // Handlers ---------------------------------------------
-  const handleChange = (event) => {
+  // Context -------------------------------------
+  // Handlers ------------------------------------
+  const handleChange = (event) => { 
     const { name, value } = event.target;
     const newValue = conformance.includes(name) ? parseInt(value) : value;
     setRecord({ ...record, [name]: newValue });
-    setErrors({
-      ...errors,
-      [name]: isValid[name](newValue) ? null : errorMessage[name],
-    });
+    setErrors({ ...errors, [name]: isValid[name](newValue) ? null : errorMessage[name]});
   };
+
   const isValidRecord = (record) => {
     let isRecordValid = true;
     Object.keys(record).forEach((key) => {
@@ -81,20 +86,19 @@ function useForm(initialRecord, conformance, { isValid, errorMessage }, onCancel
       }
     });
     return isRecordValid;
-  };
+  }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = () => { 
     isValidRecord(record) && onSubmit(record) && onCancel();
-    setErrors({ ...errors });
+    setErrors({...errors});
   };
-
-  // View -------------------------------------------------
+  
+  // View ----------------------------------------
   return [record, errors, handleChange, handleSubmit];
 }
 
-// ------------------------------------
-// Compose Form Object ////////////////
-// ------------------------------------
+// -----------------------------------------
+// Compose Form Object /////////////////////
+// -----------------------------------------
 Form.Item = Item;
 Form.useForm = useForm;
